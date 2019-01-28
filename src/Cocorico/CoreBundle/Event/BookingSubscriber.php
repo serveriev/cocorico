feaa5b4824d7399d1c37bf26b2c033a2d7441a32
lenur <lenur.serveriev@gmail.com>
2019-01-28 19:09:19 +0200

    Send an email to the administrator while new booking request.

--- src/Cocorico/CoreBundle/Event/BookingSubscriber.php
+++ src/Cocorico/CoreBundle/Event/BookingSubscriber.php
@@ -11,6 +11,8 @@
 
 namespace Cocorico\CoreBundle\Event;
 
+use Cocorico\CoreBundle\Entity\Booking;
+use Cocorico\CoreBundle\Mailer\MailerInterface;
 use Cocorico\CoreBundle\Model\Manager\BookingManager;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -20,15 +22,21 @@ class BookingSubscriber implements EventSubscriberInterface
 {
     protected $bookingManager;
     protected $dispatcher;
+    private $mailer;
 
     /**
-     * @param BookingManager           $bookingManager
-     * @param EventDispatcherInterface $dispatcher
+     * @param BookingManager            $bookingManager
+     * @param EventDispatcherInterface  $dispatcher
+     * @param MailerInterface           $mailer
      */
-    public function __construct(BookingManager $bookingManager, EventDispatcherInterface $dispatcher)
-    {
+    public function __construct(
+        BookingManager $bookingManager,
+        EventDispatcherInterface $dispatcher,
+        MailerInterface $mailer
+    ) {
         $this->bookingManager = $bookingManager;
         $this->dispatcher = $dispatcher;
+        $this->mailer = $mailer;
     }
 
 
@@ -37,12 +45,19 @@ class BookingSubscriber implements EventSubscriberInterface
      *
      * @param BookingEvent $event
      */
-    public function onBookingNewSubmitted(BookingEvent $event)
+    public function onBookingNewSubmitted(BookingEvent $event): void
     {
+        /** @var Booking $booking */
         $booking = $this->bookingManager->create($event->getBooking());
+
         if ($booking) {
             $event->setBooking($booking);
             $this->dispatcher->dispatch(BookingEvents::BOOKING_NEW_CREATED, $event);
+
+            $this->mailer->sendMessageToAdmin(
+                'A new booking was created',
+                sprintf('Booking id is %d', $booking->getId())
+            );
         }
     }
 
